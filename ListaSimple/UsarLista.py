88885f28e973f90917f39d5254072743a6517041
Josué <j.encinar.87@gmail.com>
2016-02-14 22:24:55 +0100

    Update UsarLista.py

--- ListaSimple/UsarLista.py
+++ ListaSimple/UsarLista.py
@@ -8,45 +8,45 @@ def main():
 
 
     for i in range(0,10):
-        num = random.randrange(0,30)
-        if(num < 14):
-	        lista.insertarPrimero(num)
-        elif(num%2 == 0):
-	        lista.insertarUltimo(num)
+        numero = random.randrange(0,30)
+        if(numero < 14):
+	        lista.insertarPrimero(numero)
+        elif(numero%2 == 0):
+	        lista.insertarUltimo(numero)
         else:
-	        lista.insertarOrdenado(num)
+	        lista.insertarOrdenado(numero)
 
     print("-------------------------------------------")
     print("|           Probando la lista             |")
     print("-------------------------------------------\n")
-    tam = lista.longitud()
-    print("Lista con longitud ",tam,"\n")
+    listSize = lista.longitud()
+    print("Lista no ordenada con longitud ",listSize,"\n")
     lista.visualizar()
-    print("Intentando borrar datos...\n")
+    print("\nPrueba aleatoria de borrado de 3 elementos\n")
     for i in range(3):
         borrado(lista)
     lista.ordenar()
-    tam2 = lista.longitud()
-    if(tam==tam2):
+    listSize2 = lista.longitud()
+    if(listSize==listSize2):
         print("No se han borrado datos\n")
     else:
-        print("Se borraron ",(tam-tam2)," elementos\n")
+        print("Se borraron ",(listSize-listSize2)," elemento/s\n")
          
-    print("Lista ordenada con longitud ",tam2)
+    print("Lista ordenada con longitud ",listSize2)
     lista.visualizar()
 
 def borrado(lista):
-    num = random.randrange(0,30)
+    numero = random.randrange(0,30)
 
-    if(lista.existe(num)):
+    if(lista.existe(numero)):
         if(random.randrange(1,3) == 1):
-            print("Eliminando 1 aparición de ",num," de la lista")
-            lista.eliminar(num,1)
+            print("Eliminando 1 aparicion de ",numero," de la lista")
+            lista.eliminar(numero,1)
         else:
-            print("Eliminando todas las apariciones de ",num," de la lista")
-            lista.eliminar(num,2)
+            print("Eliminando todas las apariciones de ",numero," de la lista")
+            lista.eliminar(numero,2)
     else:
-        print(num," no existe en la lista -> no se elimina")
+        print(numero," no existe en la lista -> no se elimina")
         
 
 if __name__ == '__main__':
